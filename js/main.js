e2079de51f7350173429db47893467cd573e838b
C. Saxby Bain <csaxbybain@yahoo.com>
2025-09-04 14:54:31 -0400

    Added event listener for buttons

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
+const buttons = document.querySelectorAll("button");
 const span = document.querySelector(".copyright");
-
 const d = new Date();
 const year = d.getFullYear();
-span.innerText = year; 
+span.innerText = year;
 
 
-
-
-
+buttons.forEach(button => button.addEventListener("click", function() {
+    alert("Thank you for donating to Cat Rescue!");
+}));
