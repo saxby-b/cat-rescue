3ad91c51d1df4ddce92d4abaeb61c1ef474854f0
C. Saxby Bain <csaxbybain@yahoo.com>
2025-09-26 13:59:47 -0400

    Organized code

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const d = new Date();
 const year = d.getFullYear();
 span.innerText = year;
 
-
-buttons.forEach(button => button.addEventListener("click", function() {
+buttons.forEach((button) =>
+  button.addEventListener("click", function () {
     alert("Thank you for donating to Cat Rescue!");
-}));
+  })
+);
